d4104523a6d7f13f0c02050e1371540302c27928
Chris Bieganek <cwbieganek@gmail.com>
2022-04-16 20:49:29 -0500

    Make temperature prop optional

--- src/components/Temperature.svelte
+++ src/components/Temperature.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  export let temperature: number;
+  export let temperature: number | undefined = undefined;
   let temperatureColor: string;
   $: temperatureColor = getColorForTemperature(temperature);
 
