74ee1ee29b06edef38c7bbf2d984c0eda971a863
Chris Bieganek <cwbieganek@gmail.com>
2022-04-16 20:49:37 -0500

    Initial commit

---
+++ src/components/WordSplitter.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+  let text = "";
+  let words: string[];
+  $: words = text.split(" ");
+</script>
+
+<div class="container">
+  <label for="word-splitter">Word Splitter: </label>
+  <input type="text" bind:value={text} />
+  {#each words as word, i}
+    {#if word != ""}
+      <div><span>Word {i + 1}: {word}</span></div>
+    {/if}
+  {/each}
+</div>
+
+<style>
+
+</style>
